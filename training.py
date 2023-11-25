8c4856609c5715ebd7f4053a868146a19f419690
kt201 <katiaminiaylo@gmail.com>
2023-11-25 18:51:49 +0200

    Loading training data

--- training.py
+++ training.py
@@ -1,16 +1,18 @@
+import os
 import random
 import json
 import pickle
 import numpy as np
 
 import nltk
+
+nltk.download('wordnet')
 from nltk.stem import WordNetLemmatizer
 
 import keras
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
 from keras.optimizers import SGD
-
 
 lemmatizer = WordNetLemmatizer()
 
@@ -29,5 +31,64 @@ for intent in intents["intents"]:
         if intent["tag"] not in classes:
             classes.append(intent["tag"])
 
-print(documents)
+words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
+words = sorted(set(words))
 
+classes = sorted(set(classes))
+
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
+
+training = []
+output_empty = [0] * len(classes)
+
+for document in documents:
+    bag = []
+    word_patterns = document[0]
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1
+
+    training.append([bag, output_row])
+
+random.shuffle(training)
+
+train_x = []
+train_y = []
+
+for train in training:
+    train_x.append(train[0])
+    train_y.append(train[1])
+
+train_x = np.array(train_x)
+train_y = np.array(train_y)
+
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+# Use the new optimizer without the deprecated parameters
+sgd = SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+model.fit(train_x, train_y, epochs=200, batch_size=5, verbose=1)
+
+directory_path = 'chatbot_model.model'
+
+# Check if the directory exists, and if not, create it
+if not os.path.exists(directory_path):
+    os.makedirs(directory_path)
+    print(f"Directory '{directory_path}' created successfully.")
+else:
+    print(f"Directory '{directory_path}' already exists.")
+
+# Save the model in the specified directory
+model.save(os.path.join(directory_path, 'model.h5'))
+print("Model saved successfully.")
